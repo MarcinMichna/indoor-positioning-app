0ed26b347dd8d750cbed6b1ee41fb472ef4a144b
MarcinMichna <marcinxmichna@gmail.com>
2020-10-24 19:15:01 +0200

    add json creation

--- esp/esp.ino
+++ esp/esp.ino
@@ -6,9 +6,10 @@
 #include <BLEServer.h>
 #include <BLEAdvertisedDevice.h>
 #include <Arduino_JSON.h>
+#include <HTTPClient.h>
 
 // Bluetooth server
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-c5c9c331914b"
+#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-000000000001"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
 // Access Point
@@ -23,14 +24,26 @@ const char *wifiPassword= "M@rsik353";
 int bleScanTime = 5; // in seconds
 BLEScan* bleScanner;
 
+
+// REST
+const char* serverName = "michnam.pl:5000/check";
+String json = "";
+
 class BleScanResult: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
       Serial.printf("Advertised Device: %s \n", advertisedDevice.toString().c_str());
+      String addr = advertisedDevice.getAddress().toString().c_str();
+      Serial.println(addr);
+      int rssi = advertisedDevice.getRSSI();
+      Serial.println(rssi);
+
+      json += String("{ ");
+      json += String("'addr': ") + String("'") + String(addr) + String("', ");
+      json += String("'rssi': ") + String(rssi) + String(", ");
+      json += String(" }, ");
     }
 };
 
-// REST
-const char* serverName = "michnam.pl/check";
 
 
 void setup() {
@@ -106,6 +119,7 @@ void wifiScan() {
   Serial.println("scan start");
   int n = WiFi.scanNetworks();
   Serial.println("scan done");
+  json = "{ 'wifi': [ ";
   if (n != 0) {
       for (int i = 0; i < n; ++i) {
           // Print SSID and RSSI for each network found
@@ -114,26 +128,39 @@ void wifiScan() {
           Serial.print(WiFi.SSID(i));
           Serial.print(" ");
           Serial.println(WiFi.RSSI(i));
-      }
+
+          json += String("{ ");
+          json += String("'ssid': ") + String("'") + String(WiFi.SSID(i)) + String("', ");
+          json += String("'rssi': ") + String(WiFi.RSSI(i)) + String(", ");
+          json += String("}, ");
+          }
   }
+  json = json.substring(0, json.length()-1);
+  json += String("], ");
   Serial.println(".");
   delay(1000);
 }
 
 void bleScan() {
+  json += "'ble': [ ";
   BLEScanResults foundDevices = bleScanner->start(bleScanTime, false);
   Serial.print("Devices found: ");
   Serial.println(foundDevices.getCount());
   Serial.println("Scan done!");
-  bleScanner->clearResults();   // delete results fromBLEScan buffer to release memory
+  bleScanner->clearResults(); 
+  json = json.substring(0, json.length()-1);
+  json += String("] } ");
   delay(2000);
 }
 
 void testRequest() {
   HTTPClient http;
-  http.begin(serverName);
+  http.begin("http://michnam.pl:5000/check");
   http.addHeader("Content-Type", "application/json");
-  int httpResponseCode = http.POST("{\"api_key\":\"tPmAT5Ab3j7F9\",\"sensor\":\"BME280\",\"value1\":\"24.25\",\"value2\":\"49.54\",\"value3\":\"1005.14\"}");
+  int httpResponseCode = http.POST(json);
   Serial.print("HTTP Response code: ");
   Serial.println(httpResponseCode);
+  Serial.println(json);
+  http.end();
+  json = "{ 'data': [ "; //reset json
 }
