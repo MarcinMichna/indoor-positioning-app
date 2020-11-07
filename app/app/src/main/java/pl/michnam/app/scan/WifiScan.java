09e753d577d37a44572b905578f6a0f7eb4ba493
MarcinMichna <marcinxmichna@gmail.com>
2020-11-07 17:05:51 +0100

    code refactoring

--- app/app/src/main/java/pl/michnam/app/scan/WifiScan.java
+++ app/app/src/main/java/pl/michnam/app/scan/WifiScan.java
@@ -7,58 +7,61 @@ import android.content.IntentFilter;
 import android.net.wifi.ScanResult;
 import android.net.wifi.WifiManager;
 import android.util.Log;
-import android.widget.TextView;
 
 import java.util.List;
 
-import pl.michnam.app.MainActivity;
+import pl.michnam.app.core.AppConfig;
+import pl.michnam.app.service.MainService;
 import pl.michnam.app.service.ServiceCallbacks;
+import pl.michnam.app.util.Tag;
 
 public class WifiScan {
-    private static String TAG = "inposScan";
 
-    public static void setupWifiScan(Context context, ServiceCallbacks serviceCallbacks){
-
-        WifiManager wifiManager = (WifiManager)
-                context.getSystemService(Context.WIFI_SERVICE);
+    public static void setupWifiScan(Context context, ServiceCallbacks serviceCallbacks) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
 
         BroadcastReceiver wifiScanReceiver = new BroadcastReceiver() {
             @Override
             public void onReceive(Context c, Intent intent) {
-                boolean success = intent.getBooleanExtra(
-                        WifiManager.EXTRA_RESULTS_UPDATED, false);
-                if (success) {
+                boolean noError = intent.getBooleanExtra(WifiManager.EXTRA_RESULTS_UPDATED, false);
+                if (noError) {
                     List<ScanResult> results = wifiManager.getScanResults();
-                    String info = "";
-                    for(ScanResult i : results) {
-                        info += "SSID: " + i.SSID + ", RSSI: " + i.level;
-                        Log.d(TAG,info);
-                        info += "\n";
-                    }
-                    if (serviceCallbacks != null) serviceCallbacks.printWifi(info);
-                } else {
-                    // scan failure handling
-                    Log.i(TAG, "Wifi scan error");
-                }
-                scan(context);
+                    handleScanResults(results, serviceCallbacks);
+                } else Log.w(Tag.WIFI, "WIFI - error while receiving scan results");
+                if (MainService.isWorking()) scanLoop(context);
+                else Log.i(Tag.WIFI, "WIFI - Stopping Scan");
             }
         };
 
         IntentFilter intentFilter = new IntentFilter();
         intentFilter.addAction(WifiManager.SCAN_RESULTS_AVAILABLE_ACTION);
         context.registerReceiver(wifiScanReceiver, intentFilter);
+        Log.i(Tag.WIFI, "WIFI - Strating scan");
+        if (MainService.isWorking()) scanLoop(context);
+    }
+
+    private static void scanLoop(Context context) {
+        WifiManager wifiManager = (WifiManager) context.getSystemService(Context.WIFI_SERVICE);
+        new java.util.Timer().schedule(
+                new java.util.TimerTask() {
+                    @Override
+                    public void run() {
+                        boolean successfulScan = wifiManager.startScan(); // wifiScanReceiver.onReceive after scan
+                        if (!successfulScan) Log.i(Tag.WIFI, "WIFI - Scan while starting scanning");
+                    }
+                },
+                AppConfig.wifiScanWaitTime * 1000
+        );
 
     }
 
-    public static void scan(Context context) {
-        WifiManager wifiManager = (WifiManager)
-                context.getSystemService(Context.WIFI_SERVICE);
-
-        boolean success = wifiManager.startScan();
-        if (!success) {
-            Log.i(TAG, "Wifi scan error");
+    private static void handleScanResults(List<ScanResult> results, ServiceCallbacks serviceCallbacks) {
+        StringBuilder info = new StringBuilder();
+        for (ScanResult i : results) {
+            info.append("SSID: ").append(i.SSID).append(", RSSI: ").append(i.level).append("\n");
+            //Log.v(Tag.WIFI, "SSID: " + i.SSID + ", RSSI: " + i.level);
         }
+        if (serviceCallbacks != null) serviceCallbacks.setDebugMessage(info.toString());
+        Log.v(Tag.WIFI, "WIFI - Scan successful, got " + results.size() + " results");
     }
-    
-
 }
--- app/app/src/main/java/pl/michnam/app/service/ServiceCallbacks.java
+++ app/app/src/main/java/pl/michnam/app/service/ServiceCallbacks.java
@@ -1,6 +1,5 @@
 package pl.michnam.app.service;
 
 public interface ServiceCallbacks {
-    void debug();
-    void printWifi(String msg);
+    void setDebugMessage(String msg);
 }
