28a94c72ea644b9d6b358c7f206562fec0c40f34
MarcinMichna <marcinxmichna@gmail.com>
2020-11-22 14:41:45 +0100

    clean ble server and change ble scan time to 2 sec

--- esp/esp.ino
+++ esp/esp.ino
@@ -7,12 +7,11 @@
 #include <BLEAdvertisedDevice.h>
 #include <HTTPClient.h>
 
-// Bluetooth server
-#define SERVICE_UUID        "4fafc201-1fb5-459e-8fcc-000000000001"
-#define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+int bleScanTime = 2; // in seconds
+
 
 // Access Point
-const char *apSsid = "ESP_1";
+const char *espName = "ESP_1";
 const char *apPassword= "polska123";
 
 // Wifi connection
@@ -20,7 +19,7 @@ const char *wifiSsid = "Marcin_Krul";
 const char *wifiPassword= "M@rsik353";
 
 // Bluetooth
-int bleScanTime = 5; // in seconds
+
 BLEScan* bleScanner;
 
 
@@ -30,14 +29,16 @@ String json = "";
 
 class BleScanResult: public BLEAdvertisedDeviceCallbacks {
     void onResult(BLEAdvertisedDevice advertisedDevice) {
-      
+
+      String bleName = advertisedDevice.getName().c_str();
       String addr = advertisedDevice.getAddress().toString().c_str();
       int rssi = advertisedDevice.getRSSI();
 
       json += String("{ ");
+      json += String("\"name\": ") + String("\"") + String(bleName) + String("\", ");
       json += String("\"addr\": ") + String("\"") + String(addr) + String("\", ");
       json += String("\"rssi\": ") + String(rssi) + String(", ");
-      json += String("\"esp\": ") + String("\"") + String(apSsid) + String("\" ");
+      json += String("\"esp\": ") + String("\"") + String(espName) + String("\" ");
       json += String(" },");
     }
 };
@@ -53,22 +54,20 @@ void setup() {
 }
 
 void loop() {
-  delay(1000);
+  delay(100);
   wifiScan();
   bleScan();
-  testRequest();
+  sendHttpRequest();
 }
 
 void apSetup() {
-  Serial.println(".");
   Serial.println("Setting up Wifi Access Point");
-  WiFi.softAP(apSsid, apPassword);
+  WiFi.softAP(espName, apPassword);
   IPAddress apIP = WiFi.softAPIP();
   Serial.print("Wifi AP ssid: ");
-  Serial.print(apSsid);
+  Serial.print(espName);
   Serial.print(", IP: ");
   Serial.println(apIP);
-  Serial.println(".");
 }
 
 void stationSetup() {
@@ -87,26 +86,16 @@ void bluetoothClientSetup() {
   bleScanner -> setActiveScan(true); //active scan uses more power, but get results faster
   bleScanner -> setInterval(100);
   bleScanner -> setWindow(99);  // less or equal setInterval value
-  Serial.println(".\nBluetooth client setup successful\n.");
+  Serial.println("Ble scan setup successful\n.");
 }
 
 void bluetoothServerSetup() {
-  BLEDevice::init("");
+  BLEDevice::init(espName);
   BLEServer *pServer = BLEDevice::createServer();
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-  BLECharacteristic *pCharacteristic = pService->createCharacteristic(
-                                         CHARACTERISTIC_UUID,
-                                         BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE
-                                       );
-
-  pCharacteristic->setValue("Hello World");
-  pService->start();
   BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
   pAdvertising->setScanResponse(true);
   BLEDevice::startAdvertising();
-  Serial.println(".");
+  Serial.println("Ble server setup successful\n.");
 }
 
 void wifiScan() {
@@ -119,7 +108,7 @@ void wifiScan() {
           json += String("{ ");
           json += String("\"ssid\": ") + String("\"") + String(WiFi.SSID(i)) + String("\", ");
           json += String("\"rssi\": ") + String(WiFi.RSSI(i)) + String(", ");
-          json += String("\"esp\": ") + String("\"") + String(apSsid) + String("\"");
+          json += String("\"esp\": ") + String("\"") + String(espName) + String("\"");
           json += String("},");
           }
   }
@@ -137,19 +126,18 @@ void bleScan() {
   json += String("] } ");
 }
 
-void testRequest() {
+void sendHttpRequest() {
   HTTPClient http;
   http.begin(serverName);
   http.addHeader("Content-Type", "application/json");
 
   Serial.print("Sending json: ");
-  int httpResponseCode = http.POST(json);
-  
   Serial.println(json);
-  Serial.print("Sending to: ");
-  Serial.println(serverName);
+
+  int httpResponseCode = http.POST(json);
   Serial.print("HTTP Response code: ");
   Serial.println(httpResponseCode);
   http.end();
+  
   Serial.println(".");
 }
